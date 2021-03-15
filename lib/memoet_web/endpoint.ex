3d15a607739921dcef70fefbe188f1434302a138
manhtai <vo@manhtai.com>
2021-03-15 23:20:36 +0700

    x-cloak

--- lib/memoet_web/endpoint.ex
+++ lib/memoet_web/endpoint.ex
@@ -23,7 +23,7 @@ defmodule MemoetWeb.Endpoint do
   plug Plug.Static,
     at: "/",
     from: :memoet,
-    gzip: false,
+    gzip: true,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
