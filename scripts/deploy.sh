adeb3e58770978f8d798da809167b16911e36b24
manhtai <vo@manhtai.com>
2021-03-16 17:17:39 +0700

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
+git pull origin master
 mix deps.get
 MIX_ENV=prod mix compile
 
 # Assets
-cd assets/
-npm install
-npm run deploy
-cd ..
-mix phx.digest
+(cd assets/ && npm install && npm run deploy)
+MIX_ENV=prod mix phx.digest
 
 sudo lsof -ti :80 | xargs kill
 PORT=80 MIX_ENV=prod elixir --erl "-detached" -S mix phx.server --no-compile
