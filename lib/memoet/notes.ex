fee15ad8f8cfb36ffc530d16ac8e10e6ff65f783
manhtai <vo@manhtai.com>
2021-03-20 20:40:17 +0700

    More wordings

--- lib/memoet/notes.ex
+++ lib/memoet/notes.ex
@@ -28,12 +28,12 @@ defmodule Memoet.Notes do
 
     Note
     |> where(^filter_where(params))
-    |> order_by(desc: :inserted_at)
+    |> order_by(desc: :updated_at)
     |> Repo.paginate(
       before: cursor_before,
       after: cursor_after,
       include_total_count: true,
-      cursor_fields: [{:inserted_at, :desc}],
+      cursor_fields: [{:updated_at, :desc}],
       limit: 10
     )
   end
