ba70647ce2bf36fd9a3139f48dfc748369241ef3
Mạnh Tài <vo@manhtai.com>
2021-03-31 20:23:26 +0700

    Community link (#16)

--- config/prod.exs
+++ config/prod.exs
@@ -11,8 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :memoet, MemoetWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "http", host: "memoet.com", port: 80],
-  # force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [scheme: "https", host: "memoet.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
