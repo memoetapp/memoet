f93dd3402c3f80c6daaa93a221a73274023d5e3a
manhtai <vo@manhtai.com>
2021-04-17 15:54:30 +0700

    Echo steps in deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,5 +7,7 @@ MIX_ENV=prod mix compile
 (cd assets/ && npm ci && npm run deploy)
 MIX_ENV=prod mix phx.digest
 
+echo "Restarting server..."
 (sudo lsof -ti :4000 | xargs kill) || true
 PORT=4000 MIX_ENV=prod elixir --erl "-detached" -S mix phx.server --no-compile
+echo "Server restarted!"
